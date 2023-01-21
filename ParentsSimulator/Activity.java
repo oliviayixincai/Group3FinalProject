599127816c40a247180d4e54035a4d3756538d01
oliviayixincai <oliviayixincai@gmail.com>
2023-01-20 22:01:04 -0500

    ScheduleWorld selection

--- ParentsSimulator/Activity.java
+++ ParentsSimulator/Activity.java
@@ -1,4 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.ArrayList;
 
 /**
  * Write a description of class Activity here.
@@ -8,8 +9,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Activity extends Actor
 {
+    public static final int SPEED = 20;
+
+    private int originalX;
+    private int originalY;
+    private boolean isMouseDown;
+    
     public Activity() {
-        
+        setImage(new GreenfootImage("activityExample.png"));
+    }
+    
+    public void addedToWorld(World w) {
+        originalX = getX();
+        originalY = getY();
     }
 
     /**
@@ -18,6 +30,47 @@ public class Activity extends Actor
      */
     public void act()
     {
-        // Add your action code here.
+        if (Greenfoot.mouseDragged(this)) {
+            setLocation(Greenfoot.getMouseInfo().getX(),Greenfoot.getMouseInfo().getY());
+            this.isMouseDown = true;
+        }
+        else if (Greenfoot.mouseDragEnded(this)) {
+            ArrayList<Activity> activities = (ArrayList<Activity>) getObjectsAtOffset(0, 0, Activity.class);
+            if (activities.size() == 1) {
+                ScheduleWorld sw = (ScheduleWorld) getWorld();
+                sw.trySetActivity(this);
+            }
+            this.isMouseDown = false;
+        }
+        
+        if (!this.isMouseDown) {
+            checkMove();
+        }
+    }
+    
+    private void checkMove() {
+        int x = getX();
+        int y = getY();
+        if (x != originalX || y != originalY) {
+            double dx = Math.abs(originalX - x);
+            double dy = Math.abs(originalY - y);
+            double degree = Math.atan(dy / dx);
+            double mx = Math.cos(degree) * SPEED;
+            double my = Math.sin(degree) * SPEED;
+            if (dx < mx || dy < my) {
+                setLocation(originalX, originalY);
+            }
+            else {
+                int xDirection = originalX > x ? 1 : -1;
+                int yDirection = originalY > y ? 1 : -1;
+                setLocation((int) (x + mx * xDirection), (int) (y + my * yDirection));
+            }
+        }
+    }
+    
+    public void setActivity(int x, int y) {
+        setLocation(x, y);
+        this.originalX = x;
+        this.originalY = y;
     }
 }
--- ParentsSimulator/Grid.java
+++ ParentsSimulator/Grid.java
@@ -9,19 +9,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Grid extends Actor
 {
     public Grid() {
-        GreenfootImage grid = new GreenfootImage(500, 340);
-        grid.setColor(Color.BLACK);
-        grid.drawLine(0, 0, 480, 0);
-        grid.drawLine(0, 50, 480, 50);
-        grid.drawLine(0, 140, 480, 140);
-        grid.drawLine(0, 230, 480, 230);
-        grid.drawLine(0, 320, 480, 320);
-        
-        grid.drawLine(0, 0, 0, 320);
-        grid.drawLine(120, 0, 120, 320);
-        grid.drawLine(240, 0, 240, 320);
-        grid.drawLine(360, 0, 360, 320);
-        grid.drawLine(480, 0, 480, 320);
+        GreenfootImage grid = new GreenfootImage(1000, 700);
+        int x1 = 410;
+        int y1 = 215;
+        int x2 = 890;
+        int y2 = 485;
+        int height = 90;
+        int width = 120;
+        for (int i = 0; i < (y2 - y1) / height + 1; i++) {
+            grid.drawLine(x1, y1 + i * height, x2, y1 + i * height);
+        }
+        for (int i = 0; i < (x2 - x1) / width + 1; i++) {
+            grid.drawLine(x1 + i * 120, y1, x1 + i * 120, y2);
+        }
         setImage(grid);
     }
     /**
