1ccffb7ddd8d74193ff9361045b1629716d0777a
YuxinLi88 <116744852+YuxinLi88@users.noreply.github.com>
2023-01-21 22:28:34 -0500

    add user info

--- ParentsSimulator/End_World.java
+++ ParentsSimulator/End_World.java
@@ -9,12 +9,19 @@ import java.util.Arrays;
  */
 public class End_World extends World
 {
+    private Player player;
+    private UserInfo user;
+    private int highScore;
+    private String bestJob;
+    private int score;
+    
     private Map<String, Integer> stat_bars=new HashMap<String, Integer>();
     private int[] stats_values; 
     private String first;
     private String second;
     private String job;
     private boolean display=false;
+    
     /**
      * Constructor for objects of class End_World.
      * 
@@ -29,14 +36,31 @@ public class End_World extends World
         stat_bars.put("Creativity", stats[3]);
         stats_values=stats;
         Arrays.sort(stats_values);
+        
+        if (UserInfo.isStorageAvailable()) { // check if connected
+            user = UserInfo.getMyInfo();
+        }
+        if (user != null){ // check if logged in
+            highScore = user.getScore();
+            bestJob = user.getString(1);
+            //scoreBar.update("Welcome " + user.getUserName() + "! Don't Die! High score: " + highScore);
+
+        } else {
+            //scoreBar.update("Please Login to Play with full features!");
+        }
     }
+    
     public void act(){
         if(display==false){
             findMax();
             find_job();
             display=true;
         }
+        if(Greenfoot.mouseClicked(this)){
+            endGame();
+        }
     }
+    
     public void findMax(){
         for(String key: stat_bars.keySet()) {
             if(stat_bars.get(key)==stats_values[3]){
@@ -53,12 +77,13 @@ public class End_World extends World
             }
         }
     }
+    
     public void find_job(){
         if((first.equals("IQ")||second.equals("IQ"))&&(first.equals("EQ")||second.equals("EQ"))){
             job="doctor";
         }
         if((first.equals("IQ")||second.equals("IQ"))&&(first.equals("Creativity")||second.equals("Creativity"))){
-            job="game_developer";
+            job="programmer";
         }
         if((first.equals("IQ")||second.equals("IQ"))&&(first.equals("Memory")||second.equals("Memory"))){
             job="lawyer";
@@ -73,5 +98,32 @@ public class End_World extends World
             job="actor";
         }
         setBackground(new GreenfootImage(job+".png"));
+    }
+    
+    public void stopped(){
+        user.store();
+    }
+    
+    //store user info
+    private void endGame() {
+        // calculate score by adding up all stats
+        for(int i: stats_values){
+            score += i;
+        }
+
+        if (score > highScore){
+            highScore = score;
+            bestJob = job;
+        }
+        //the kid's best job is the job with highest score
+        if (user != null){
+            user.setScore(highScore);
+            user.setString(1, bestJob);
+            user.store();
+        }
+
+        addObject (new ScoreBoard(720, 480), 512, 320);
+        
+        Greenfoot.stop();   
     }
 }
