c5d44791327273df58ea57648c8e028047ed4731
YuxinLi88 <116744852+YuxinLi88@users.noreply.github.com>
2023-01-22 21:18:58 -0500

    comments

--- ParentsSimulator/Stats_Board.java
+++ ParentsSimulator/Stats_Board.java
@@ -3,7 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Stats_Board here.
  * 
- * @Yuxin (your name) 
+ * @Yuxin Li(your name) 
  * @version (a version number or a date)
  */
 public class Stats_Board extends Actor
